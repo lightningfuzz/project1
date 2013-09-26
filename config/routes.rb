a81fb95415cef987ea4b7c5c15fe5f75cfb74418
Michael <michaelsanter@me.com>
2013-09-26 10:33:39 -0700

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Project1::Application.routes.draw do
+  get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,5 @@ Project1::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'pages#home'
 end
